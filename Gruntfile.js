faf3cc5c91895c8e2a4099e0f3f9e493e496856d
bms <mikesoylu@gmail.com>
2013-05-15 02:59:44 +0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+var folderMount = function folderMount(connect, point) {
+  return connect.static(path.resolve(point) + "/out"); // set server path to out
+};
+
+module.exports = function( grunt ) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    coffee: {
+      compile: {
+        src: ['src/*.coffee'],
+        dest: 'out/main.js'
+      }
+    },
+
+    livereload: {
+      port: 35729 // Default livereload listening port.
+    },
+
+    connect: {
+      livereload: {
+        options: {
+          port: 9000,
+          middleware: function(connect, options) {
+            return [lrSnippet, folderMount(connect, options.base)]
+          }
+        }
+      }
+    },
+
+    regarde: {
+      coffee: {
+        files:['src/*.coffee'],
+        tasks:['coffee']
+      },
+
+      reload: {
+        files:['out/*'],
+        tasks:['livereload']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+
+  grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
+
+};
---
+++ README.md
@@ -0,0 +1,24 @@
+CoffeeScript Grunt Boilerplate
+===
+
+This is intended for games or small apps that are interactive.
+
+## Getting Started
+
+If you haven't used [grunt][] before, be sure to check out the [Getting Started][] guide. Firstly you need to install [grunt-cli][];
+
+```shell
+npm install -g grunt-cli
+```
+Then you need to install the packages needed for this project;
+
+```shell
+npm install
+```
+Additionally you need the chrome [autoreload][] plugin, or a similar one for other browsers. Onece you are ready fire up [grunt][]!
+
+```shell
+grunt
+```
+
+Thats it! Happy coding!
